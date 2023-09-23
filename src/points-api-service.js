6d9c6e1e38d360a07694f60d90e3db0f473a61f9
Anatolii <volotovskiy111@gmail.com>
2023-09-23 09:51:55 +0300

    Создает класс для работы с API

---
+++ src/points-api-service.js
@@ -0,0 +1,26 @@
+import ApiService from './framework/api-service';
+
+const Method = {
+  GET: 'GET',
+  PUT: 'PUT',
+};
+
+export default class PointsApiService extends ApiService {
+  get tasks() {
+    return this._load({url: 'points'})
+      .then(ApiService.parseResponse);
+  }
+
+  async updatePoint(point) {
+    const response = await this._load({
+      url: `points/${point.id}`,
+      method: Method.PUT,
+      body: JSON.stringify(point),
+      headers: new Headers({'Content-Type': 'application/json'})
+    });
+
+    const parseResponse = await ApiService.parseResponse(response);
+
+    return parseResponse;
+  }
+}
